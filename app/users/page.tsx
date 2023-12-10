c747772909ce1c1b564395e2521551c5fd5f8a1f
0x1m4o <chuntdevnx@gmail.com>
2023-12-11 02:16:43 +0700

    Data Fetching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,48 @@
 import React from "react";
+interface Users {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
 
-const UsersPage = () => {
+interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+interface Geo {
+  lat: string;
+  lng: string;
+}
+
+interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data: Users[] = await res.json();
   return (
     <>
       <h1>Users Page</h1>
+      {data.length > 0 ? (
+        <ul>
+          {data.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <h1>Error</h1>
+      )}
     </>
   );
 };
